351cdcb4be8c6cff1d457435d4fc18070f2d002c
JuanJo-Silva <125352475+JuanJo-Silva@users.noreply.github.com>
2026-01-14 11:51:36 +0100

    Update Managers.cpp
    add synchronizations to manager

--- src/kokkos/Managers.cpp
+++ src/kokkos/Managers.cpp
@@ -19,7 +19,18 @@ void initialize(const Params &params, ElectroMagn &em,
               << "\n"
               << std::endl;
 
+    em.sync(minipic::host, minipic::device);
+    for (std::size_t is = 0; is < particles.size(); ++is) {
+      particles[is].sync(minipic::host, minipic::device);
+    }
+    
     operators::interpolate(em, particles);
+    
+    em.sync(minipic::host, minipic::device);
+    for (std::size_t is = 0; is < particles.size(); ++is) {
+      particles[is].sync(minipic::host, minipic::device);
+    }
+    
     operators::push_momentum(particles, -0.5 * params.dt);
   }
 }
@@ -35,12 +46,22 @@ void iterate(const Params &params, ElectroMagn &em,
     DEBUG("  -> stop reset current");
   }
 
+  em.sync(minipic::host, minipic::device);
+  for (std::size_t is = 0; is < particles.size(); ++is) {
+    particles[is].sync(minipic::host, minipic::device);
+  }
+
   // Interpolate from global field to particles
   DEBUG("  -> start interpolate ");
 
   operators::interpolate(em, particles);
 
   DEBUG("  -> stop interpolate");
+
+  em.sync(minipic::host, minipic::device);
+  for (std::size_t is = 0; is < particles.size(); ++is) {
+    particles[is].sync(minipic::host, minipic::device);
+  }
 
   // Push all particles
   DEBUG("  -> start push ");
